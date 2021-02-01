042df5f43fc37293e1e2427f16697bee4ea607be
Bill Israel <bill.israel@gmail.com>
2021-01-31 18:27:00 -0600

    Allow for user-supplied configuration file

--- feedify/__main__.py
+++ feedify/__main__.py
@@ -1,21 +1,27 @@
+import argparse
 from functools import partial
 import json
 from pathlib import Path
 from wsgiref import simple_server
 
+from . import __version__ as version
 from .handler import FeedifyHandler
 
 feeds_file_cache = Path('.feeds')
 if not feeds_file_cache.is_dir():
     feeds_file_cache.mkdir(0o755)
 
-# TODO: Give this more of a CLI interface
-config_path = Path('config.json')
+parser = argparse.ArgumentParser(description='feedify: a command line website-to-RSS application.')
+parser.add_argument('--config', dest='config_file', type=str, default='config.json',
+                    help='A user-supplied configuration file.')
+parser.add_argument('--version', action='version', version='feedify {}'.format(version))
+args = parser.parse_args()
+
+config = {}
+config_path = Path(args.config_file).expanduser()
 if config_path.exists():
     with open(config_path, 'rb') as f:
         config = json.load(f)
-else:
-    config = {}
 
 server = simple_server.WSGIServer(('', 8000), partial(FeedifyHandler, config))
 server.serve_forever()
